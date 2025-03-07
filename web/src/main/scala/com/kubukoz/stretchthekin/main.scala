0edf3eac666925843359dd2329a387c51aaea079
Jakub Kozłowski <kubukoz@gmail.com>
2025-03-07 01:16:48 +0100

    Extract file

--- web/src/main/scala/com/kubukoz/stretchthekin/main.scala
+++ web/src/main/scala/com/kubukoz/stretchthekin/main.scala
@@ -3,15 +3,12 @@ package com.kubukoz.stretchthekin
 import calico.IOWebApp
 import calico.html.io.*
 import calico.html.io.given
-import cats.derived.*
 import cats.effect.IO
 import cats.effect.kernel.Resource
-import cats.kernel.Eq
 import cats.syntax.all.*
 import fs2.concurrent.Signal
 import fs2.concurrent.SignallingRef
 import fs2.dom.HtmlElement
-import language.experimental.namedTuples
 import monocle.Focus
 import monocle.syntax.all.*
 
@@ -70,108 +67,5 @@ object App extends IOWebApp {
   //     Step.Liftoffs(10),
   //   )
   // )
-
-}
-
-object CountdownComponent {
-
-  enum CurrentCounter derives Eq {
-    case Paused
-    case Running(since: FiniteDuration, now: FiniteDuration)
-
-    def runningTotal: FiniteDuration =
-      this match {
-        case Running(since, now) => now - since
-        case Paused              => 0.seconds
-      }
-
-  }
-
-  case class State(current: CurrentCounter, stored: FiniteDuration) {
-
-    private val cutoff = 0.seconds
-
-    def finished: Boolean = total <= cutoff
-    def total: FiniteDuration = (stored - current.runningTotal) max cutoff
-
-    def paused: Boolean = current === CurrentCounter.Paused
-
-  }
-
-  def render(
-    countdownFrom: FiniteDuration,
-    refreshRate: FiniteDuration,
-    onFinished: IO[Unit],
-  ): Resource[IO, HtmlElement[IO]] = SignallingRef[IO]
-    .of(State(current = CurrentCounter.Paused, stored = countdownFrom))
-    .toResource
-    .flatMap { state =>
-      val total = state.map(_.total)
-      val paused = state.map(_.paused)
-      val current = state.lens(_.current)
-      val finished = state.map(_.finished)
-
-      val switch = IO.realTime.flatMap { now =>
-        state.update { s =>
-          s.current.match {
-            case CurrentCounter.Paused =>
-              s.copy(
-                current = CurrentCounter.Running(now, now)
-              )
-
-            case r: CurrentCounter.Running =>
-              s.copy(
-                current = CurrentCounter.Paused,
-                stored = s.total,
-              )
-          }
-        }
-      }
-
-      val updateState =
-        fs2
-          .Stream
-          .fixedDelay[IO](refreshRate)
-          .interruptWhen(finished)
-          .foreach { _ =>
-            IO.realTime.flatMap { now =>
-              current.update {
-                case r: CurrentCounter.Running => r.copy(now = now)
-                case c                         => c
-              }
-            }
-          }
-          .compile
-          .drain
-          .background
-          .void
-
-      val notifyFinished =
-        finished
-          .discrete
-          .filter(identity)
-          .foreach(_ => onFinished)
-          .compile
-          .drain
-          .background
-          .void
-
-      div(
-        "Current time: ",
-        total.map { e =>
-          s"${e.toSeconds}:${e.toMillis % 1000}"
-        },
-        button(
-          disabled <-- finished,
-          paused.map {
-            case true  => "Resume"
-            case false => "Pause"
-          },
-          onClick(switch),
-        ),
-        updateState,
-        notifyFinished,
-      )
-    }
 
 }
